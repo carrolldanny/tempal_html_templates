a0ee86b0abbab60caea26cda861aaaa222509fdf
Faraz92 <ftahir@tssg.org>
2016-05-17 12:39:39 +0100

    Welcome screen and Dashboard

---
+++ bootstrap/js/header_load.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+    //console.log( "document loaded - loading bg image now" );
+    $.backstretch("./images/welcomeimages/Backheader.jpg");
+});
+
+$( window ).load(function() {
+    //console.log( "window loaded" );
+});
